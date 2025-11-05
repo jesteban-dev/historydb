72d75cbfbb3d2c0215bc61b695c251f210e26a6f
jesteban-dev <jesteban-dev@proton.me>
2025-11-05 15:45:25 +0100

    REFACTOR: usecases & handlers

--- src/internal/usecases/backup_usecases_impl.go
+++ src/internal/usecases/backup_usecases_impl.go
@@ -128,7 +128,7 @@ func (uc *BackupUsecasesImpl) RollbackSnapshot(first bool) {
 	fmt.Println("Closing app...")
 }
 
-func (uc *BackupUsecasesImpl) BackupSchemaDependencies(lastSnapshot, snapshot *entities.BackupSnapshot) bool {
+func (uc *BackupUsecasesImpl) BackupSchemaDependencies(snapshot *entities.BackupSnapshot) bool {
 	dbReader := uc.dbFactory.CreateReader()
 	backupWriter := uc.backupFactory.CreateWriter()
 
@@ -142,49 +142,14 @@ func (uc *BackupUsecasesImpl) BackupSchemaDependencies(lastSnapshot, snapshot *e
 	// Backups all schema dependencies
 	schemaDependenciesProgress := progressbar.NewOptions(len(schemaDependencies), progressbar.OptionSetDescription(fmt.Sprintf("  + Saving all %d schema dependencies...", len(schemaDependencies))), progressbar.OptionSetWidth(30), progressbar.OptionSetWriter(os.Stdout), progressbar.OptionSetRenderBlankState(true))
 	for _, dependency := range schemaDependencies {
-		if lastSnapshot != nil {
-			backupReader := uc.backupFactory.CreateReader()
-
-			// If no condition it stays unmodified
-			hash := dependency.Hash()
-			prevHash, ok := lastSnapshot.SchemaDependencies[dependency.GetName()]
-			if !ok {
-				// New dependency to write into backup
-				if err := backupWriter.SaveSchemaDependency(dependency); err != nil {
-					uc.logger.Errorf("could not save %s schema dependency into backup: %v\n", dependency.GetName(), err)
-					return false
-				}
-
-				snapshot.SchemaDependencies[dependency.GetName()] = hash
-			} else if !crypto.CompareHashes(prevHash, hash) {
-				// Updates dependency into backup
-				prevDependency, err := backupReader.GetSchemaDependency(prevHash)
-				if err != nil {
-					if errors.Is(err, services.ErrBackupCorruptedFile) {
-						fmt.Printf("The %s schema dependency in backup is corrupted\n", dependency.GetName())
-					}
-					uc.logger.Errorf("could not read %s schema dependency from backup: %v\n", dependency.GetName(), err)
-					return false
-				}
-
-				dependencyDiff := dependency.Diff(prevDependency)
-				if err := backupWriter.SaveSchemaDependencyDiff(dependencyDiff); err != nil {
-					uc.logger.Errorf("could not update %s schema dependency into backup: %v\n", dependency.GetName(), err)
-					return false
-				}
-
-				snapshot.SchemaDependencies[dependency.GetName()] = fmt.Sprintf("diffs/%s", hash)
-			}
-		} else {
-			// Saves dependency into backup
-			if err := backupWriter.SaveSchemaDependency(dependency); err != nil {
-				uc.logger.Errorf("could not save %s schema dependency into backup: %v\n", dependency.GetName(), err)
-				return false
-			}
-
-			// Saves ref into snapshot
-			snapshot.SchemaDependencies[dependency.GetName()] = dependency.Hash()
+		// Saves dependency into backup
+		if err := backupWriter.SaveSchemaDependency(dependency); err != nil {
+			uc.logger.Errorf("could not save %s schema dependency into backup: %v\n", dependency.GetName(), err)
+			return false
 		}
+
+		// Saves ref into snapshot
+		snapshot.SchemaDependencies[dependency.GetName()] = dependency.Hash()
 		schemaDependenciesProgress.Add(1)
 	}
 
@@ -192,7 +157,60 @@ func (uc *BackupUsecasesImpl) BackupSchemaDependencies(lastSnapshot, snapshot *e
 	return true
 }
 
-func (uc *BackupUsecasesImpl) BackupSchemas(lastSnapshot, snapshot *entities.BackupSnapshot) []entities.Schema {
+func (uc *BackupUsecasesImpl) SnapshotSchemaDependencies(lastSnapshot, snapshot *entities.BackupSnapshot) bool {
+	dbReader := uc.dbFactory.CreateReader()
+	backupReader := uc.backupFactory.CreateReader()
+	backupWriter := uc.backupFactory.CreateWriter()
+
+	// List schema dependencies from database
+	schemaDependencies, err := dbReader.ListSchemaDependencies()
+	if err != nil {
+		uc.logger.Errorf("could not list schema dependencies from DB: %v\n", err)
+		return false
+	}
+
+	// Backups all schema dependencies
+	schemaDependenciesProgress := progressbar.NewOptions(len(schemaDependencies), progressbar.OptionSetDescription(fmt.Sprintf("  + Updating all %d schema dependencies...", len(schemaDependencies))), progressbar.OptionSetWidth(30), progressbar.OptionSetWriter(os.Stdout), progressbar.OptionSetRenderBlankState(true))
+	for _, dependency := range schemaDependencies {
+		// If no condition it stays unmodified
+		hash := dependency.Hash()
+		prevHash, ok := lastSnapshot.SchemaDependencies[dependency.GetName()]
+		if !ok {
+			// New dependency to write into backup
+			if err := backupWriter.SaveSchemaDependency(dependency); err != nil {
+				uc.logger.Errorf("could not save %s schema dependency into backup: %v\n", dependency.GetName(), err)
+				return false
+			}
+
+			snapshot.SchemaDependencies[dependency.GetName()] = hash
+		} else if !crypto.CompareHashes(prevHash, hash) {
+			// Updates dependency into backup
+			prevDependency, err := backupReader.GetSchemaDependency(prevHash)
+			if err != nil {
+				if errors.Is(err, services.ErrBackupCorruptedFile) {
+					fmt.Printf("The %s schema dependency in backup is corrupted\n", dependency.GetName())
+				}
+				uc.logger.Errorf("could not read %s schema dependency from backup: %v\n", dependency.GetName(), err)
+				return false
+			}
+
+			dependencyDiff := dependency.Diff(prevDependency)
+			if err := backupWriter.SaveSchemaDependencyDiff(dependencyDiff); err != nil {
+				uc.logger.Errorf("could not update %s schema dependency into backup: %v\n", dependency.GetName(), err)
+				return false
+			}
+
+			snapshot.SchemaDependencies[dependency.GetName()] = fmt.Sprintf("diffs/%s", hash)
+		}
+
+		schemaDependenciesProgress.Add(1)
+	}
+
+	fmt.Println("  - All schema dependencies updated successfully!")
+	return true
+}
+
+func (uc *BackupUsecasesImpl) BackupSchemas(snapshot *entities.BackupSnapshot) []entities.Schema {
 	dbReader := uc.dbFactory.CreateReader()
 	backupWriter := uc.backupFactory.CreateWriter()
 
@@ -215,53 +233,81 @@ func (uc *BackupUsecasesImpl) BackupSchemas(lastSnapshot, snapshot *entities.Bac
 		}
 		hash := schema.Hash()
 
-		if lastSnapshot != nil {
-			backupReader := uc.backupFactory.CreateReader()
+		schemas = append(schemas, schema)
 
-			// If no condition it stays unmodified
-			prevHash, ok := lastSnapshot.Schemas[schemaName]
-			if !ok {
-				// New schema to write into backup
-				if err := backupWriter.SaveSchema(schema); err != nil {
-					uc.logger.Errorf("could not save %s schema definition into backup: %v\n", schemaName, err)
-					return nil
-				}
+		// Saves schema into the backup
+		if err := backupWriter.SaveSchema(schema); err != nil {
+			uc.logger.Errorf("could not save %s schema definition into backup: %v\n", schemaName, err)
+			return nil
+		}
 
-				snapshot.Schemas[schemaName] = hash
-			} else if !crypto.CompareHashes(prevHash, hash) {
-				// Updates schema into backup
-				prevSchema, err := backupReader.GetSchema(prevHash)
-				if err != nil {
-					if errors.Is(err, services.ErrBackupCorruptedFile) {
-						fmt.Printf("The %s schema in backup is corrupted\n", schema.GetName())
-					}
+		// Saves the schema ref into the snapshot
+		snapshot.Schemas[schemaName] = hash
+		schemaProgress.Add(1)
+	}
 
-					uc.logger.Errorf("could not read %s schema definition from backup: %v\n", schemaName, err)
-					return nil
-				}
+	fmt.Println("  - All schema definitions saved successfully")
+	return schemas
+}
 
-				schemaDiff := schema.Diff(prevSchema)
-				if err := backupWriter.SaveSchemaDiff(schemaDiff); err != nil {
-					uc.logger.Errorf("could not update %s schema definition into backup: %v\n", schemaName, err)
-					return nil
-				}
+func (uc *BackupUsecasesImpl) SnapshotSchemas(lastSnapshot, snapshot *entities.BackupSnapshot) []entities.Schema {
+	dbReader := uc.dbFactory.CreateReader()
+	backupReader := uc.backupFactory.CreateReader()
+	backupWriter := uc.backupFactory.CreateWriter()
 
-				snapshot.Schemas[schemaName] = fmt.Sprintf("diffs/%s", hash)
-			}
-		} else {
-			schemas = append(schemas, schema)
+	// Lists schemas from database
+	schemaNames, err := dbReader.ListSchemaNames()
+	if err != nil {
+		uc.logger.Errorf("could not list schemas from DB: %v\n", err)
+		return nil
+	}
 
-			// Saves schema into the backup
+	// Backups all schema definitions
+	schemas := make([]entities.Schema, 0, len(schemaNames))
+	schemaProgress := progressbar.NewOptions(len(schemaNames), progressbar.OptionSetDescription(fmt.Sprintf("  + Saving all %d schema definitions...", len(schemaNames))), progressbar.OptionSetWidth(30), progressbar.OptionSetWriter(os.Stdout), progressbar.OptionSetRenderBlankState(true))
+	for _, schemaName := range schemaNames {
+		// Reads schema definition
+		schema, err := dbReader.GetSchemaDefinition(schemaName)
+		if err != nil {
+			uc.logger.Errorf("could not retrieve %s schema definition from DB: %v\n", schemaName, err)
+			return nil
+		}
+		hash := schema.Hash()
+
+		// If no condition it stays unmodified
+		prevHash, ok := lastSnapshot.Schemas[schemaName]
+		if !ok {
+			// New schema to write into backup
 			if err := backupWriter.SaveSchema(schema); err != nil {
 				uc.logger.Errorf("could not save %s schema definition into backup: %v\n", schemaName, err)
 				return nil
 			}
 
-			// Saves the schema ref into the snapshot
 			snapshot.Schemas[schemaName] = hash
+		} else if !crypto.CompareHashes(prevHash, hash) {
+			// Updates schema into backup
+			prevSchema, err := backupReader.GetSchema(prevHash)
+			if err != nil {
+				if errors.Is(err, services.ErrBackupCorruptedFile) {
+					fmt.Printf("The %s schema in backup is corrupted\n", schema.GetName())
+				}
+
+				uc.logger.Errorf("could not read %s schema definition from backup: %v\n", schemaName, err)
+				return nil
+			}
+
+			schemaDiff := schema.Diff(prevSchema)
+			if err := backupWriter.SaveSchemaDiff(schemaDiff); err != nil {
+				uc.logger.Errorf("could not update %s schema definition into backup: %v\n", schemaName, err)
+				return nil
+			}
+
+			snapshot.Schemas[schemaName] = fmt.Sprintf("diffs/%s", hash)
 		}
+
 		schemaProgress.Add(1)
 	}
-	fmt.Println("  - All schema definitions saved successfully")
+
+	fmt.Println("  - All schema definitions updated successfully")
 	return schemas
 }
